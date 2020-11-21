78a9374b1e49434366cb65ce8b483a6fe23813f8
Jose Lopez <lezep2@gmail.com>
2020-11-21 12:07:35 -0800

    added IntegerPalindrome checking method

---
+++ javaInterviews/IntegerPalindrome.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class IntegerPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number = ");
+
+        int toCheck = input.nextInt();
+
+    }
+
+    public static boolean isIntPalindrome(int n) {
+
+        int number = n;
+
+        int temp = 0;
+        int sum = 0;
+        while (number != 0) {
+
+            number = number / 10;
+
+        }
+        return false;
+
+    }
+}
