71c4bce9e7c02200c7d37abb41075006ea122072
Jose Lopez <lezep2@gmail.com>
2020-11-20 21:52:51 -0800

    added substring division without built in function

--- javaInterviews/PalindromeChecker.java
+++ javaInterviews/PalindromeChecker.java
@@ -9,16 +9,34 @@ public class PalindromeChecker {
         System.out.print("Enter a string = ");
         toCheck = input.nextLine();
 
+        if (is_Palindrome(toCheck))
+            System.out.println(toCheck + " is a Palindrome");
+        else
+            System.out.println(toCheck + " is not a Palindrome");
+
     }
 
-    public static boolean isPalindrome(String s) {
+    public static boolean is_Palindrome(String s) {
 
-        int length = s.length();
-        for (int i = 0; i < s.length() / 2; i++) {
+        int length = s.length() - 1;
+        for (int i = 0; i < (s.length() / 2); i++) {
             if (s.charAt(i) != s.charAt(length - i)) {
                 return false;
             }
         }
         return true;
     }
-}
+
+    public static boolean is_Palindrome_recursive(String s) {
+
+        String subString = "";
+        for(int i = 1; i<s.length()-1;i++)
+        {
+
+        }
+        if (s.length() == 1 || s.length() == 0) {
+            return true;
+        }
+        else if()
+        }
+}}
