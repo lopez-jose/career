7b84e16b3984baea40459f72f9509e483783a9db
Jose Lopez <lezep2@gmail.com>
2020-11-20 22:01:46 -0800

    added working recursive method for isPalindrome

--- javaInterviews/PalindromeChecker.java
+++ javaInterviews/PalindromeChecker.java
@@ -9,7 +9,7 @@ public class PalindromeChecker {
         System.out.print("Enter a string = ");
         toCheck = input.nextLine();
 
-        if (is_Palindrome(toCheck))
+        if (is_Palindrome_recursive(toCheck))
             System.out.println(toCheck + " is a Palindrome");
         else
             System.out.println(toCheck + " is not a Palindrome");
@@ -28,15 +28,16 @@ public class PalindromeChecker {
     }
 
     public static boolean is_Palindrome_recursive(String s) {
-
-        String subString = "";
-        for(int i = 1; i<s.length()-1;i++)
-        {
-
-        }
         if (s.length() == 1 || s.length() == 0) {
             return true;
         }
-        else if()
+
+        String subString = "";
+        for (int i = 1; i < s.length() - 1; i++) {
+            subString += s.charAt(i);
         }
-}}
+        if (s.charAt(0) != s.charAt(s.length() - 1))
+            return false;
+        return is_Palindrome_recursive(subString);
+    }
+}
