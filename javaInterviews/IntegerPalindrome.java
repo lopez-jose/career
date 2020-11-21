123a16967b36877aabdd52b08b30c8f8f2fe4a2c
Jose Lopez <lezep2@gmail.com>
2020-11-21 12:52:55 -0800

    added working implementation of integer palindrome

--- javaInterviews/IntegerPalindrome.java
+++ javaInterviews/IntegerPalindrome.java
@@ -11,19 +11,34 @@ public class IntegerPalindrome {
 
         int toCheck = input.nextInt();
 
+        if (isIntPalindrome(toCheck))
+            System.out.println("Number is a Palindrome");
+        else
+            System.out.println("Number is not a Palindrome");
+
+        input.close();
     }
 
     public static boolean isIntPalindrome(int n) {
 
         int number = n;
 
-        int temp = 0;
-        int sum = 0;
+        int digit = 0;
+        String s = "";
         while (number != 0) {
 
+            digit = number % 10;
             number = number / 10;
 
+            s = s + digit;
+            System.out.print(digit + " ");
+
         }
+
+        System.out.println(s);
+        if (Integer.valueOf(s) == n)
+            return true;
+
         return false;
 
     }
