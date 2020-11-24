f6f13525c06906f1b6d392ba3f7505d0ffb29498
Jose Lopez <lezep2@gmail.com>
2020-11-23 21:26:38 -0800

    added hash mapping system that adds chars to map

---
+++ javaInterviews/AnagramChecker.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        System.out.println("This program prints out if two words are anagrams");
+
+        String word1 = "mary";
+        String word2 = "army";
+
+        System.out.println(isAnagram(word1, word2));
+
+    }
+
+    public static boolean isAnagram(String word1, String word2) {
+
+        char[] characters1 = word1.toCharArray();
+
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        Map<Character, Integer> charMap1 = new HashMap<Character, Integer>();
+        Map<Character, Integer> charMap2 = new HashMap<Character, Integer>();
+
+        for (Character ch : characters1) {
+            if (charMap1.containsKey(ch)) {
+                charMap1.put(ch, charMap1.get(ch) + 1);
+                charMap2.put(ch, charMap2.get(ch) + 1);
+            } else {
+                charMap1.put(ch, 1);
+                charMap2.put(ch, 1);
+            }
+
+        }
+
+        System.out.println(charMap1.size());
+        System.out.println(charMap2.size());
+
+        Set<Map.Entry<Character, Integer>> entrySet = charMap1.entrySet();
+
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            if (entry.getValue() > 1) {
+
+            }
+        }
+
+        return false;
+    }
+}
