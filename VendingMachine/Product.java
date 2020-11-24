037bcd30f1e3ca7eacacc433afe35f9b03d85d35
Jose Lopez <lezep2@gmail.com>
2020-11-23 23:52:41 -0800

    added product attributes

---
+++ VendingMachine/Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    public String name;
+    public int price;
+    public int quantity;
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+}
