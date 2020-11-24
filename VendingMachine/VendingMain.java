0ed628d8a536f006d4be3ab2b2f32d069b04cf73
Jose Lopez <lezep2@gmail.com>
2020-11-24 00:14:37 -0800

    changed filename from VendingMachine.java to VendingMain.java

--- VendingMachine/VendingMain.java
+++ VendingMachine/VendingMain.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class VendingMachine {
+public class VendingMain {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("This program simulates a vending machine");
 
         ArrayList<Product> items = new ArrayList<Product>();
         Product coke = new Product("Coke", 25, 10);
+        Product pepsi = new Product("Pepsi", 35, 10);
+        Product soda = new Product("Soda", 45, 10);
 
         items.add(coke);
+        items.add(pepsi);
+        items.add(soda);
+
         Vending a = new Vending(3, items);
         Vending.printProducts(items);
 
+        System.out.println("What would you like to order?");
+
+        input.close();
+
     }
+
 }
