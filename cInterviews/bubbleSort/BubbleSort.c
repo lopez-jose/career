8493f04a66c32795259c8dfe78df92dba9383314
Jose Lopez <lezep2@gmail.com>
2020-12-01 11:04:08 -0800

    added skeleton code of bubblesort

---
+++ cInterviews/bubbleSort/BubbleSort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap()
+{
+}
+void bubbleSort(int array[], int n)
+{
+    for (int i < 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (array[j] > array[j])
+                swap()
+}
+int main()
+{
+    int array[] = {1, 5, 2, 5, 0, 3};
+    //get length of array
+    int n = sizeof(array) / sizeof(array[0]);
+    return 0;
+}
