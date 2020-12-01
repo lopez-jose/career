0947189099392347b4c39c384391d024f22d3ce2
Jose Lopez <lezep2@gmail.com>
2020-12-01 12:25:42 -0800

    added working implementation of executable

--- cInterviews/bubbleSort/BubbleSort.c
+++ cInterviews/bubbleSort/BubbleSort.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-void swap()
+void swap(int *xp, int *yp)
 {
+    int temp = *xp;
+    //printf("%d ", temp);
+
+    *xp = *yp;
+    *yp = temp;
+}
+void printArray(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", array[i]);
+    printf("\n");
 }
 void bubbleSort(int array[], int n)
 {
-    for (int i < 0; i < n - 1; i++)
-        for (j = 0; j < n - i - 1; j++)
-            if (array[j] > array[j])
-                swap()
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j + 1]);
+                printArray(array, n);
+            }
+        printf("End of loop %d\n", i);
+    }
 }
 int main()
 {
     int array[] = {1, 5, 2, 5, 0, 3};
     //get length of array
     int n = sizeof(array) / sizeof(array[0]);
+    bubbleSort(array, n);
+
     return 0;
 }
