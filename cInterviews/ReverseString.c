dfa646b116de9b2c23bb5fc4cd140e52bd90a01d
Jose Lopez <lezep2@gmail.com>
2020-11-30 23:42:40 -0800

    added compiled code

--- cInterviews/ReverseString.c
+++ cInterviews/ReverseString.c
@@ -4,6 +4,20 @@
 void reverseString(char *str)
 {
     int l, i;
+    char *begin_ptr, *end_ptr;
 
-    l = strlen
+    l = strlen(str);
+
+    begin_ptr = str;
+    end_ptr = str;
+}
+
+int main()
+{
+
+    printf("Hello world");
+    char str[100] = "hello";
+    //reverseString(&s);
+
+    return 0;
 }
