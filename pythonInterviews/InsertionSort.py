435dc657de50f3b228075a6b190ea41ae78fe605
Jose Lopez <lezep2@gmail.com>
2020-12-03 21:27:17 -0800

    added working implementation of insertion sort

--- pythonInterviews/InsertionSort.py
+++ pythonInterviews/InsertionSort.py
@@ -1,14 +1,20 @@
 def InsertionSort(arr):
     print("hello")
     A = []
-    arr
 
     for i in range(len(arr)):
         min_idx = i
         for j in range(i+1, len(arr)):
             if arr[min_idx] > arr[j]:
                 min_idx = j
-        A[i], A[]
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        printSortStep(arr)
+
+
+def printSortStep(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print("\n")
 
 
 def main():
