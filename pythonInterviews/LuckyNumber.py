67f9b98efed4fda3c914fa422ba1d0ab0b4b10b6
Jose Lopez <lezep2@gmail.com>
2020-12-04 21:24:40 -0800

    added changes to math.floor

--- pythonInterviews/LuckyNumber.py
+++ pythonInterviews/LuckyNumber.py
@@ -7,13 +7,15 @@ def isLucky(num):
     array = [0]*10
 
     temp = num
+    print(temp)
     while temp > 0:
-        temp = math.floor(temp % 10)
-        if array[temp] == 0:
-            array[temp] = 1
+        print(temp)
+        digit = math.floor(temp % 10)
+        if (array[digit] == 0):
+            array[digit] = 1
         else:
             return 0
-        temp = temp/10
+        temp = (temp/10)
 
     return lucky
 
