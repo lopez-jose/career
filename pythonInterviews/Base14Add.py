50041d1ed63d6bebf6e4529b53ffffe48d021f9e
Jose Lopez <lezep2@gmail.com>
2020-12-05 14:45:00 -0800

    created rajmethod to convert base

---
+++ pythonInterviews/Base14Add.py
@@ -0,0 +1,17 @@
+
+
+def convertToBase10(num, base):
+    return 0
+
+
+def rajMethod(num1, num2):
+    return 0
+
+
+def main():
+
+    num1 = input("base 14 number = ")
+    num2 = input("base 14 number = ")
+
+
+main()
