97627e136e16ba753e3725d6335fc2d32874217f
Jose Lopez <lezep2@gmail.com>
2020-12-05 15:45:02 -0800

    added correct base10 conversion using dictionary

--- pythonInterviews/Base14Add.py
+++ pythonInterviews/Base14Add.py
@@ -1,6 +1,20 @@
+convertValue = {
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15
+}
 
 
 def convertToBase10(num, base):
+    # The base will be started with
+    sum = 0
+    for i in range(len(num)):
+        base10 = convertValue.get(num[i], num[i])
+        print(base10)
+
     return 0
 
 
@@ -10,8 +24,10 @@ def rajMethod(num1, num2):
 
 def main():
 
-    num1 = input("base 14 number = ")
-    num2 = input("base 14 number = ")
+   # num1 = input("base 14 number = ")
+    #num2 = input("base 14 number = ")
+    convertToBase10("AAA", 14)
+    print(convertValue.get('A', 0))
 
 
 main()
