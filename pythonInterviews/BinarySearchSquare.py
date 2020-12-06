1043d5386fb6acff7d5a1c38a9737b969d322ce8
Jose Lopez <lezep2@gmail.com>
2020-12-05 21:31:00 -0800

    modified binary search with different boundary conditions

--- pythonInterviews/BinarySearchSquare.py
+++ pythonInterviews/BinarySearchSquare.py
@@ -1,18 +1,37 @@
 import math
 
 
-def binarysearch():
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def binarySearch(num, limit, increment):
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    start = 0
+    end = len(array)-1
+    middle = (start+end)/2
+    startEqualsEnd = False
+
+    while startEqualsEnd == False:
+        if start == end:
+            startEqualsEnd = True
+            return array[middle]
+        if((num+increment*array[middle]**2 > limit)):
+            end = middle
 
 
 def binarySquareRoot(toFind, precision):
     lessThan = True
-    while(lessThan)
+    i = 1
+    while(i**2 < toFind):
+        i = i + 1
+        # print(i)
+    digit = i-1
+    increment = 0.1
+    binarySearch(digit, toFind, increment)
+    return digit
 
 
 def main():
     toFind = 123
     precision = 5
+    print(binarySquareRoot(toFind, precision))
 
 
 main()
