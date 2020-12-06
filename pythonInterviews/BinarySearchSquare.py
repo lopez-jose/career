c7ad6163b7712e4752466ed597ed92fbf7cffa01
Jose Lopez <lezep2@gmail.com>
2020-12-05 20:21:10 -0800

    added binarySearchRoot method

---
+++ pythonInterviews/BinarySearchSquare.py
@@ -0,0 +1,18 @@
+import math
+
+
+def binarysearch():
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def binarySquareRoot(toFind, precision):
+    lessThan = True
+    while(lessThan)
+
+
+def main():
+    toFind = 123
+    precision = 5
+
+
+main()
