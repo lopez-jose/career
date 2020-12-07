982c7729a0fc047bc97e3e1768324d1209e83ca9
Jose Lopez <lezep2@gmail.com>
2020-12-06 16:48:06 -0800

    added comparison between linearSearch time and binarySearch time

--- pythonInterviews/LinearSearchvsBubbleSearch.py
+++ pythonInterviews/LinearSearchvsBubbleSearch.py
@@ -7,10 +7,37 @@ def linearSearch(array, numSearch):
         if(array[i] == numSearch):
             millisEnd = int(round(time.time()*1000))
             return i, millisEnd-millisStart
+    millisEnd = int(round(time.time()*1000))
+    return -1, millisEnd
 
 
 def binarySearch(array, numSearch):
-    millisStart = int(roun)
+    millisStart = int(round(time.time()*1000))
+    isFound = False
+
+    start = 0
+    end = len(array)-1
+    middle = (end+start)//2
+
+    while isFound == False:
+        print("running")
+        if(array[middle] == numSearch):
+            millisEnd = int(round(time.time()*1000))
+            return middle, millisEnd-millisStart
+        if middle == end or middle == start:
+            millisEnd = int(round(time.time()*1000))
+            if(array[end] == numSearch):
+                return end, millisEnd-millisStart
+            if(array[start] == numSearch):
+                return start, millisEnd-millisStart
+            return -1, millisEnd-millisStart
+        if(array[middle] > numSearch):
+            end = middle
+            middle = (end+start)//2
+        else:
+            start = middle
+            middle = (end+start)//2
+
 
 def main():
 
@@ -26,6 +53,10 @@ def main():
 
     print("Linear Search: %s found at Position: %s in %s ms" %
           (maxValue, position, timeLinear))
+
+    position, timeBinary = binarySearch(array, maxValue)
+    print("Binary Search: %s found at Position: %s in %s ms" %
+          (maxValue, position, timeBinary))
 
 
 main()
