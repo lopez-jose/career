2550f2ab08e83ba8a669b907688021c71803d264
Jose Lopez <lezep2@gmail.com>
2020-12-07 22:15:40 -0800

    changed recursivebinarySearch method

--- pythonInterviews/InterpolationSearchvsBinarySearch.py
+++ pythonInterviews/InterpolationSearchvsBinarySearch.py
@@ -43,27 +43,22 @@ def binarySearch(array, left, right, searchNum):
 
 
 def recursiveBinarySearch(array, left, right, searchNum):
-    millisStart = int(round(time.time()*1000))
-
-    while(right >= 1):
+    if(right >= 1):
         middle = (left+(right-1))//2
 
         if(array[middle] == searchNum):
             return middle
-        if(array[middle] < searchNum):
-            millisEnd = int(round(time.time()*1000))
+        if(array[middle] > searchNum):
 
-            return recursiveBinarySearch(array, middle+1, right, searchNum), millisEnd-millisStart
-        else:
-            millisEnd = int(round(time.time()*1000))
-            return recursiveBinarySearch(array, left, middle-1, searchNum), millisEnd-millisStart
+            return recursiveBinarySearch(array, left, middle-1, searchNum)
+        return recursiveBinarySearch(array, left, middle+1, searchNum)
 
     return -1
 
 
 def main():
     array = []
-    maxValue = 100000000
+    maxValue = 10
     for i in range(1, maxValue):
         array.append(i)
     position, timeInterpol = interpolationSearch(array, len(array), maxValue-1)
@@ -76,10 +71,9 @@ def main():
     print("Binary Search: Number found at position: %s, in %s ms" %
           (position, timeBinary))
 
-    position, timeRecursiveBinary = recursiveBinarySearch(
+    position = recursiveBinarySearch(
         array, 0, len(array), maxValue-1)
-    print("Recursive Binary Search, Number found at position: %s, in %s ms" %
-          (position, timeRecursiveBinary))
+    print("Recursive Binary Search, Number found at position:%s" % (position))
 
 
 main()
