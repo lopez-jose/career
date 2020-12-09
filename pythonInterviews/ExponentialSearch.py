747bd2070cd5432b10d014e6b422d0706ec91efc
Jose Lopez <lezep2@gmail.com>
2020-12-08 18:58:25 -0800

    added working implementation of exponential search

--- pythonInterviews/ExponentialSearch.py
+++ pythonInterviews/ExponentialSearch.py
@@ -21,12 +21,14 @@ def exponentialSearch(array, left, right, searchNum):
     while searching:
         index = (int(math.pow(2, search)))
         print(index)
-        if(search < right):
+        if(index < right):
             if(searchNum < array[index]):
-                binarySearch(array, left, right, searchNum)
+                print(binarySearch(array, left, index, searchNum))
                 searching = False
         else:
+            print(binarySearch(array, left, right, searchNum))
             searching = False
+        search = search+1
 
 
 def main():
@@ -35,9 +37,9 @@ def main():
     for i in range(0, maxValue):
         array.append(i)
 
-    position = binarySearch(array, 0, len(array), maxValue-2)
-
-    print("Number %s is found at position %s" % (maxValue-1, position))
+    # position = binarySearch(array, 0, len(array), maxValue-2)
+    exponentialSearch(array, 0, maxValue, (maxValue-1))
+    # print("Number %s is found at position %s" % (maxValue-1, position))
 
 
 main()
