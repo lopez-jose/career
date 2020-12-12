6d41d0739a6dd73b384ff4c94f2027794c3d409c
Jose Lopez <lezep2@gmail.com>
2020-12-12 11:32:35 -0800

    added corrrect code for flipping the largest item to the end of a list

--- pythonInterviews/PancakeSort.py
+++ pythonInterviews/PancakeSort.py
@@ -7,6 +7,7 @@ def pancakeFlip(array):
     print("Largest Position = %s" % (largest))
 
     arrayFlipped = flip(array, largest)
+    arrayFlipped = flip(array, len(array)-1)
     # print(arrayFlipped)
 
     # returns a flipped array from 0 to i
@@ -14,14 +15,16 @@ def pancakeFlip(array):
 
 def flip(array, n):
     arrayFlipped = []
-    end = n-1
+    end = n
     start = 0
-    for i in range(n//2):
+    for i in range((n+1)//2):
+        print("Start = %s End = %s n = %s" % (start, end, (n+1)//2))
         temp = array[end]
         array[end] = array[start]
         array[start] = temp
         end = end - 1
         start = start + 1
+
         printArray(array)
         entered = input("Press Enter for next step")
     return arrayFlipped
@@ -36,6 +39,7 @@ def printArray(array):
 def main():
     array = [1, 2, 3, 4, 2, 2]
     printArray(array)
+    print("Now here")
 
     pancakeFlip(array)
 
