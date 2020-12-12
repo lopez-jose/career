dd7b36c0839d0e550aa123792cb2d25857071891
Jose Lopez <lezep2@gmail.com>
2020-12-12 08:46:35 -0800

    added printArray method

--- pythonInterviews/PancakeSort.py
+++ pythonInterviews/PancakeSort.py
@@ -1,15 +1,24 @@
+def pancakeFlip(array, i):
+    return 0
 
 
 def flip(array, i):
+    arrayFlipped = []
     for i in reversed(range(len(array))):
-        print(array[i], end=" ")
-    print("\n")
+        arrayFlipped.append(array[i])
+
+    printArray(array)
+
+
+def printArray(array):
+    for i in range(len(array)):
+        print(array[i], end=' ')
 
 
 def main():
     array = [1, 5, 3, 29, 3, 1, 5, 2, 5, 388, 92, 39, 5]
 
-    flip(array, len(array))
+    arrayFlipped = flip(array, len(array))
 
 
 main()
