fb99c9b564096e2dd8f2bdbe60d3ff869c9909bb
Jose Lopez <lezep2@gmail.com>
2020-12-12 10:42:11 -0800

    added start of pancakeFlip method

--- pythonInterviews/PancakeSort.py
+++ pythonInterviews/PancakeSort.py
@@ -1,24 +1,43 @@
-def pancakeFlip(array, i):
-    return 0
+def pancakeFlip(array):
+    largest = array[0]
+    for i in range(1, len(array)):
+        if(array[i] > largest):
+            largest = i
+
+    print("Largest Position = %s" % (largest))
+
+    arrayFlipped = flip(array, largest)
+    # print(arrayFlipped)
+
+    # returns a flipped array from 0 to i
 
 
-def flip(array, i):
+def flip(array, n):
     arrayFlipped = []
-    for i in reversed(range(len(array))):
-        arrayFlipped.append(array[i])
-
-    printArray(array)
+    end = n-1
+    start = 0
+    for i in range(n//2):
+        temp = array[end]
+        array[end] = array[start]
+        array[start] = temp
+        end = end - 1
+        start = start + 1
+        printArray(array)
+        entered = input("Press Enter for next step")
+    return arrayFlipped
 
 
 def printArray(array):
     for i in range(len(array)):
         print(array[i], end=' ')
+    print("\n")
 
 
 def main():
-    array = [1, 5, 3, 29, 3, 1, 5, 2, 5, 388, 92, 39, 5]
+    array = [1, 2, 3, 4, 2, 2]
+    printArray(array)
 
-    arrayFlipped = flip(array, len(array))
+    pancakeFlip(array)
 
 
 main()
