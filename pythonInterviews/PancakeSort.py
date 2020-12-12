c523a8d295c492eaeb3df1a4ceeb6b2eaf6c33c8
Jose Lopez <lezep2@gmail.com>
2020-12-12 15:02:41 -0800

    added working pancake sort method

--- pythonInterviews/PancakeSort.py
+++ pythonInterviews/PancakeSort.py
@@ -1,26 +1,27 @@
 def pancakeFlip(array):
 
-    arrayFlipped = []
     n = len(array)
     while(n >= 1):
         largest = array[0]
-        for i in range(0, n):
-            if(array[i] > largest):
+        for i in range(1, n):
+            if(array[i] >= largest):
                 largest = i
+        print("Array = ", end=" ")
+        printArray(array)
+        print("Largest Position = %s n = %s" % (largest, n))
 
-        print("Largest Position = %s" % (largest))
-
-        arrayFlipped = flip(array, largest)
-        print("Result after flip(arr,mi) = ", end=" ")
-        printArray(arrayFlipped)
-        arrayFlipped = flip(array, n-1)
-        print("Result after flip(arry,curr_size-1) = ", end=" ")
-        printArray(arrayFlipped)
+        if(largest != n-1):
+            array = flip(array, largest)
+            print("Result after flip(arr,mi) = ", end=" ")
+            printArray(array)
+            array = flip(array, n-1)
+            print("Result after flip(arry,curr_size-1) = ", end=" ")
+            printArray(array)
         n = n - 1
         entered = input("Enter")
 
     print("Final = ", end=' ')
-    print(arrayFlipped)
+    print(array)
 
 
 def flip(array, n):
@@ -28,14 +29,14 @@ def flip(array, n):
     end = n
     start = 0
     for i in range((n+1)//2):
-        #print("Start = %s End = %s n = %s" % (start, end, (n+1)//2))
+        # print("Start = %s End = %s n = %s" % (start, end, (n+1)//2))
         temp = array[end]
         array[end] = array[start]
         array[start] = temp
         end = end - 1
         start = start + 1
 
-        #entered = input("Press Enter for next step")
+        # entered = input("Press Enter for next step")
     return array
 
 
@@ -52,6 +53,10 @@ def main():
     printArray(array)
 
     pancakeFlip(array)
+
+   # array = [1, 2, 3, 4, 5, 6]
+    # flip(array, len(array)-4)
+    printArray(array)
 
 
 main()
