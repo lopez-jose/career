c7e1b12b0dcdcdc3387d8f4f158ad3edc95b1161
Jose Lopez <lezep2@gmail.com>
2020-12-12 18:43:56 -0800

    cleaned up pancakeSort code

--- pythonInterviews/PancakeSort.py
+++ pythonInterviews/PancakeSort.py
@@ -2,26 +2,22 @@ def pancakeFlip(array):
 
     n = len(array)
     while(n >= 1):
-        largest = array[0]
-        for i in range(1, n):
-            if(array[i] >= largest):
-                largest = i
-        print("Array = ", end=" ")
-        printArray(array)
-        print("Largest Position = %s n = %s" % (largest, n))
+        largest = getLargest(array, n)
 
         if(largest != n-1):
             array = flip(array, largest)
-            print("Result after flip(arr,mi) = ", end=" ")
-            printArray(array)
             array = flip(array, n-1)
-            print("Result after flip(arry,curr_size-1) = ", end=" ")
-            printArray(array)
         n = n - 1
-        entered = input("Enter")
+    return array
 
-    print("Final = ", end=' ')
-    print(array)
+
+def getLargest(array, n):
+    maxIndex = 0
+    for i in range(0, n):
+        if array[i] > array[maxIndex]:
+            maxIndex = i
+
+    return maxIndex
 
 
 def flip(array, n):
@@ -29,14 +25,11 @@ def flip(array, n):
     end = n
     start = 0
     for i in range((n+1)//2):
-        # print("Start = %s End = %s n = %s" % (start, end, (n+1)//2))
         temp = array[end]
         array[end] = array[start]
         array[start] = temp
         end = end - 1
         start = start + 1
-
-        # entered = input("Press Enter for next step")
     return array
 
 
