c621224e2f89c947398ad24e371dd34b56983d48
Jose Lopez <lezep2@gmail.com>
2020-12-13 12:52:59 -0800

    added working insertionSort code

---
+++ pythonInterviews/InsertionSortTest.py
@@ -0,0 +1,28 @@
+
+def InsertionSort(array):
+
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
+        while j >= 0 and key < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+
+
+def printInsertionSortStep(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print("\n")
+
+
+def main():
+    print("This program runs insertionSort on an array")
+
+    array = [12, 11, 13, 5, 6]
+    InsertionSort(array)
+
+    print(array)
+
+
+main()
