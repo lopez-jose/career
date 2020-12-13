15780a55d7e892067ac74a9fd4849acd9b1eed4b
Jose Lopez <lezep2@gmail.com>
2020-12-13 15:54:07 -0800

    added final changes

--- pythonInterviews/InsertionSortWithBinarySort.py
+++ pythonInterviews/InsertionSortWithBinarySort.py
@@ -25,13 +25,25 @@ def insertion_sort(array):
         searchNum = array[i]
         # the value where you put the number
         # the boundes of binary search are inputted
+
         j = binary_search(array, 0, i-1, searchNum)
+        print(j)
         array = array[:j]+[searchNum]+array[j:i]+array[i+1:]
+        print_array(array)
     return array
 
 
+def print_array(array):
+    for i in range(len(array)):
+        print(array[i], end=' ')
+    print("\n")
+
+
 def main():
-    return 0
+    array = [4, 2, 3, 2]
+    insertion_sort(array)
+
+    print(array)
 
 
 main()
