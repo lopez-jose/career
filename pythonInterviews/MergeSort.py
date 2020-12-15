89bd64d5b108e46cb31a952111e4e25fc60d7c1f
Jose Lopez <lezep2@gmail.com>
2020-12-14 20:52:37 -0800

    added merge_sort logic

--- pythonInterviews/MergeSort.py
+++ pythonInterviews/MergeSort.py
@@ -1,7 +1,31 @@
 
-def merge_sort(array, left, right):
-    if(right > left):
-        middle = (left+right)
+def merge_sort(array):
+    if(len(array) > 1):
+        middle = len(array)//2
+
+        # these are subarrays of the original array
+        left = array[:middle]
+        right = array[middle:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if(array[left] < array[right]):
+                array[k] = array[left]
+            else:
+                array[k] = array[right]
+            k += 1
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        while i < len(right):
+            array[k] = right[i]
+            i += 1
+            k += 1
 
     return 0
 
