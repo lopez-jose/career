0ef73bc42e8dc43e3f855900e62167079600b592
Jose Lopez <lezep2@gmail.com>
2020-12-14 20:07:22 -0800

    added array definition to follow in pseudocode

--- pythonInterviews/MergeSort.py
+++ pythonInterviews/MergeSort.py
@@ -1,10 +1,13 @@
 
-def merge_sort():
+def merge_sort(array, left, right):
+    if(right > left):
+        middle = (left+right)
 
     return 0
 
 
 def main():
+    array = [38, 27, 43, 3, 9, 82, 10]
     return 0
 
 
