6eeff637c05b327ffba167c384ca060c181bae93
Jose Lopez <lezep2@gmail.com>
2020-12-16 20:41:17 -0800

    added n_cubed and n_squared

--- clrs/1.1CLRS.py
+++ clrs/1.1CLRS.py
@@ -3,6 +3,18 @@ time_secs = [1, 60, 3600, 3600*24, 3600 *
              24*30, 3600*24*30*365, 3600*24*30*365*100]
 
 
+def calculate_two_n(number):
+    computation_count = 0
+    computations = 1
+    while(computations < number):
+        computation_count += 1
+        print(computations)
+        computations = 2**computation_count
+
+    print("\n")
+    return computation_count-1
+
+
 def calculate_log_n(number):
     return 0
 
@@ -19,16 +31,34 @@ def calculate_n_log_n(number):
     return computation_count*time_secs[0], computation_count*time_secs[1], computation_count*time_secs[2], computation_count*time_secs[3], computation_count*time_secs[4], computation_count*time_secs[5], computation_count*time_secs[6]
 
 
+def calculate_n_squared(number):
+    n = 1
+    computations = 0
+    while(computations < number):
+        computations = n**2
+        n += 1
+    return n-1
+
+
+def calculate_n_cubed(number):
+    n = 1
+    computations = 0
+    while(computations < number):
+        computations = n**3
+        n += 1
+    return n-1
+
+
 def calculate_n_factorial(number):
-    computation_count = 1
+    computation_count = 0
     computations = 1
     while(computations < number):
-        computations = computations*computation_count
         computation_count += 1
+        print(computations)
+        computations = computations*computation_count
 
-    # print(computation_count)
-
-    return computation_count*time_secs[0], computation_count*time_secs[1], computation_count*time_secs[2], computation_count*time_secs[3], computation_count*time_secs[4], computation_count*time_secs[5], computation_count*time_secs[6]
+    print("\n")
+    return computation_count-1
 
 
 def main():
@@ -37,7 +67,13 @@ def main():
     print("\n")
     print(calculate_n_log_n(1000000))
 
-    return 0
+    print("Factorial limit = %s" % (calculate_n_factorial(1000000)))
+
+    print("Two to the N limit = %s" % (calculate_two_n(1000000)))
+
+    print("N squared = %s" % (calculate_n_squared(1000000)))
+
+    print("N Cubed= %s" % (calculate_n_cubed(1000000)))
 
 
 main()
