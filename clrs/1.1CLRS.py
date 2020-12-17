d0e2b7a4faf8c945e9be4e06ca46e0bd4ed0bbb8
Jose Lopez <lezep2@gmail.com>
2020-12-16 16:15:47 -0800

    added calculate_n_log_n function

--- clrs/1.1CLRS.py
+++ clrs/1.1CLRS.py
@@ -1,9 +1,22 @@
 import math
 
 
+def calculate_n_log_n(number):
+    computation_count = 1
+    computations = computation_count*math.log(computation_count, 2)
+    while(computations < number):
+        computation_count += 1
+        computations = computation_count*math.log(computation_count, 2)
+
+    print(computation_count)
+
+    return computation_count
+
+
 def main():
     x = math.log(8, 2)
     print(x)
+    calculate_n_log_n(1000000)
     return 0
 
 
