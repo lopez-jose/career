a7bf7559fa734c6396dee29dfbeaf381a40283ce
Jose Lopez <lezep2@gmail.com>
2020-12-19 12:58:15 -0800

    added new children to the root of the tree

--- javaInterviews/BinaryTree.java
+++ javaInterviews/BinaryTree.java
@@ -8,4 +8,19 @@ class BinaryTree {
     BinaryTree() {
         root = null;
     }
+
+    public static void main(String[] args) {
+        // creates a new root initializes to null
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+
+        /*
+         * The following is the tree after the above statement
+         * 
+         * 1 / \ null null
+         */
+
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+    }
 }
