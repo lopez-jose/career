abc7d0345328466daaf12cc6751f6bb120af71ba
Jose Lopez <lezep2@gmail.com>
2020-12-19 14:14:41 -0800

    added addRecursive method(Node d, int value)

--- javaInterviews/BinaryTree.java
+++ javaInterviews/BinaryTree.java
@@ -9,10 +9,34 @@ class BinaryTree {
         root = null;
     }
 
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        }
+        if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        }
+        return current;
+
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
     public static void main(String[] args) {
         // creates a new root initializes to null
         BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
+        tree.add(6);
+        tree.add(4);
+        tree.add(8);
+        tree.add(3);
+        tree.add(5);
+        tree.add(7);
+        tree.add(9);
 
         /*
          * The following is the tree after the above statement
@@ -20,7 +44,5 @@ class BinaryTree {
          * 1 / \ null null
          */
 
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
     }
 }
