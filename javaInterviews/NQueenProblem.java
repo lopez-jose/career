bcd622bc0b096c58fe80563a14948b05b387a8a8
Jose Lopez <lezep2@gmail.com>
2020-12-20 21:16:51 -0800

    added check for a safe position on the board isSafe

--- javaInterviews/NQueenProblem.java
+++ javaInterviews/NQueenProblem.java
@@ -11,6 +11,25 @@ public class NQueenProblem {
     }
 
     boolean isSafe(int board[][], int row, int col) {
-
+        int i, j;
+        // returns false if a queen cannot be placed in the row[]col[]
+        for (i = 0; i < col; i++) {
+            if (board[i][col] == 1) {
+                return false;
+            }
+        }
+        // now we check the diagonals to see if there are collisions
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        // now we check the lower diagonal on the left side
+        for (i = row, j = col; j >= 0 && i <= N; i++, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
