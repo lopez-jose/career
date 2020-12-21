27d6746d0495a7ce4e9ee1b854539a6c91a77ab7
Jose Lopez <lezep2@gmail.com>
2020-12-20 16:37:53 -0800

    added print Paths declaration

---
+++ javaInterviews/PrintPaths.java
@@ -0,0 +1,9 @@
+public class PrintPaths {
+    public static void main(String[] args) {
+
+    }
+
+    public void printPaths(int mat[][], int m, int n, int i, int j, int path[], int index) {
+
+    }
+}
