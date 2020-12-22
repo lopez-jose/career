715e4c5327e90de6d94e125eeb689db8fb8f2f2f
Jose Lopez <lezep2@gmail.com>
2020-12-22 14:41:43 -0800

    added left_binary_searchand right_binary_search

--- javaInterviews/TwoSumUniquePairs.java
+++ javaInterviews/TwoSumUniquePairs.java
@@ -4,7 +4,7 @@ public class TwoSumUniquePairs {
 
     public static void main(String[] args) {
 
-        int nums[] = { 1, 1, 2, 45, 46, 46, 59, 23, 39, 53, 99, 98, 97, 96, 95 };
+        int nums[] = { 1, 1, 2, 45, 46, 46, 46, 46 };
         Arrays.sort(nums);
         printArray(nums);
     }
@@ -14,23 +14,52 @@ public class TwoSumUniquePairs {
             System.out.printf("%d ", nums[i]);
         }
         System.out.println();
-        System.out.println(right_bound(nums));
+        System.out.println(classic_binary_search(nums, 46));
+        System.out.println(right_binary_search(nums, 46));
     }
 
-    public static int right_bound(int nums[], int limit) {
-        int i = 0, j = num.length, middle;
-        while (i < j) {
-            middle = (i + j) / 2;
+    public static int classic_binary_search(int nums[], int limit) {
+        int low = 0, high = nums.length - 1, middle;
+        while (low < high) {
+            middle = (low + high) / 2;
             if (nums[middle] == limit) {
                 return middle;
             }
             if (nums[middle] > limit) {
-                j = middle;
+                high = middle - 1;
             } else {
-                i = middle + 1;
+                low = middle + 1;
             }
         }
         return -1;
 
+    }
+
+    public static int right_binary_search(int nums[], int limit) {
+        int low = 0, high = nums.length - 1, middle;
+        int count = 0;
+        while (low < high) {
+            middle = (low + high) / 2;
+            if (limit < nums[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+            System.out.println(count++);
+        }
+        return high;
+    }
+
+    public static int left_binary_search(int nums[], int limit) {
+        int low = 0, high = nums.length - 1, middle;
+        while (low < high) {
+            middle = (low + high) / 2;
+            if (limit > nums[middle]) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return low;
     }
 }
