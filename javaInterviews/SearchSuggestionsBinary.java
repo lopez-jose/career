9ae39a047f0012895da52b6b8063be06b05d1246
Jose Lopez <lezep2@gmail.com>
2020-12-21 19:50:45 -0800

    added Binary Search solution

--- javaInterviews/SearchSuggestionsBinary.java
+++ javaInterviews/SearchSuggestionsBinary.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.*;
 
 class SearchSuggestionsBinary {
 
@@ -15,5 +17,28 @@ class SearchSuggestionsBinary {
         }
         return i;
 
+    }
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        // We are supposed to return a list of lists;
+        Arrays.sort(products);
+        List<List<String>> result = new ArrayList<>();
+        int start = 0, bsStart = 0, n = products.length;
+        String prefix = new String();
+        // This is the prefix which we will be searching
+        for (char c : searchWord.toCharArray()) {
+            prefix += c;
+            start = lower_bound(products, bsStart, prefix);
+            result.add(new ArrayList<>());
+            for (int i = start; i < Math.min(start + 3, n); i++) {
+                if (products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix))
+                    break;
+                // Here we add this result if it's not greater in length than the prefi and it
+                // equals a the prefix for the length of the prefix
+                result.get(result.size() - 1).add(products[i]);
+            }
+            bsStart = Math.abs(start);
+        }
+        return result;
     }
 }
