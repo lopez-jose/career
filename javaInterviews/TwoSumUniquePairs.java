d6e2d26db69c0547dd31d1ce36022256e5bd132f
Jose Lopez <lezep2@gmail.com>
2020-12-22 14:09:16 -0800

    added binarySearch

--- javaInterviews/TwoSumUniquePairs.java
+++ javaInterviews/TwoSumUniquePairs.java
@@ -4,7 +4,7 @@ public class TwoSumUniquePairs {
 
     public static void main(String[] args) {
 
-        int nums[] = { 1, 1, 2, 45, 46, 46 };
+        int nums[] = { 1, 1, 2, 45, 46, 46, 59, 23, 39, 53, 99, 98, 97, 96, 95 };
         Arrays.sort(nums);
         printArray(nums);
     }
@@ -14,11 +14,23 @@ public class TwoSumUniquePairs {
             System.out.printf("%d ", nums[i]);
         }
         System.out.println();
+        System.out.println(right_bound(nums));
     }
 
-    public static int right_bound(int num[], int limit) {
-        int i = 0, j = num.length, mid;
+    public static int right_bound(int nums[], int limit) {
+        int i = 0, j = num.length, middle;
+        while (i < j) {
+            middle = (i + j) / 2;
+            if (nums[middle] == limit) {
+                return middle;
+            }
+            if (nums[middle] > limit) {
+                j = middle;
+            } else {
+                i = middle + 1;
+            }
+        }
+        return -1;
 
-        
     }
 }
