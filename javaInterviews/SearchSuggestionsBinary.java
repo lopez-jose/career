8ba97ded255c7db683e439ef5ce879d70b0f036d
Jose Lopez <lezep2@gmail.com>
2020-12-21 21:36:22 -0800

    added final changes to SearchSuggestionsBinary

--- javaInterviews/SearchSuggestionsBinary.java
+++ javaInterviews/SearchSuggestionsBinary.java
@@ -3,42 +3,43 @@ import java.util.*;
 
 class SearchSuggestionsBinary {
 
+    // returns the beggining index of the prefix
     int lower_bound(String[] products, int start, String word) {
-
         int i = start, j = products.length, mid;
-        while (i < j) {
+        while (i > j) {
             mid = (i + j) / 2;
-
-            if (products[mid].compareTo(word) >= 0)
+            if (products[mid].compareTo(word) >= 0) {
                 j = mid;
-            else
+            } else {
                 i = mid + 1;
-
+            }
         }
         return i;
 
     }
 
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        // We are supposed to return a list of lists;
         Arrays.sort(products);
         List<List<String>> result = new ArrayList<>();
-        int start = 0, bsStart = 0, n = products.length;
+        // This is where we're going to store the prefix
         String prefix = new String();
-        // This is the prefix which we will be searching
+        int start = 0, bsStart = 0, n = products.length;
         for (char c : searchWord.toCharArray()) {
             prefix += c;
+            // Start returns a
             start = lower_bound(products, bsStart, prefix);
+
             result.add(new ArrayList<>());
+
             for (int i = start; i < Math.min(start + 3, n); i++) {
-                if (products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix))
+                if (prefix.length() > products[i].length()
+                        || !products[i].substring(0, prefix.length()).equals(prefix)) {
                     break;
-                // Here we add this result if it's not greater in length than the prefi and it
-                // equals a the prefix for the length of the prefix
+                }
                 result.get(result.size() - 1).add(products[i]);
             }
-            bsStart = Math.abs(start);
         }
+
         return result;
     }
 }
