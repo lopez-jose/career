d862303f312594be5cb4ad8bdd16a2b23e76945a
Jose Lopez <lezep2@gmail.com>
2020-12-24 10:42:21 -0800

    added test for LongestSubstringWithoutRepeats

--- javaInterviews/LongestSubstringWithoutRepeats.java
+++ javaInterviews/LongestSubstringWithoutRepeats.java
@@ -13,30 +13,34 @@ public class LongestSubstringWithoutRepeats {
         Arrays.fill(array, -1);
         char currentChar = s.charAt(0);
         int index = 0;
-        int length = 0; 
-        int maxLength = 0; 
-        while(index<s.length())
-        {
-            if(array[currentChar]==-1)
-            {
+        int length = 0;
+        int maxLength = 0;
+        while (index < s.length() - 1) {
+            if (array[currentChar] == -1) {
                 length++;
-            }else{
-                if(index-array[currentChar]>length)
-                {
+            } else {
+                if (index - array[currentChar] > length) {
                     length++;
-                }else{
-                    length = index-array[currentChar];
+                } else {
+                    length = index - array[currentChar];
                 }
 
             }
-            if(length > maxLength))
-            {
-                maxLength 
+            if (length > maxLength) {
+                maxLength = length;
             }
+            array[currentChar] = index++;
+            currentChar = s.charAt(index);
         }
 
+        return maxLength;
+    }
 
-        return 0;
+    @Test
+    public void testLongestSubstringWithoutRepeats() {
+        String s = "pwwkew";
+        assertEquals(3, lengthOfLongestSubstring(s));
+
     }
 
 }
