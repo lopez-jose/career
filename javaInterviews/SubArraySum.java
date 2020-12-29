b92738ce8cf65c32f89dfe85c964e5b0a30a74e7
Jose Lopez <lezep2@gmail.com>
2020-12-29 15:10:21 -0800

    added test check for subarraySub

---
+++ javaInterviews/SubArraySum.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+
+public class SubArraySum {
+    public int subarraySum(int[] nums, int k) {
+        int sumArray[] = new int[nums.length + 1];
+
+        return 0;
+    }
+
+    @Test
+    public void checkifValid() {
+
+    }
+}
