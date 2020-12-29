322a90457a8b175005bb79db835d964abdf14b59
Jose Lopez <lezep2@gmail.com>
2020-12-29 11:40:17 -0800

    added test method to validate twosum method

--- javaInterviews/TwoSumArraySorted.java
+++ javaInterviews/TwoSumArraySorted.java
@@ -1,4 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 public class TwoSumArraySorted {
 
@@ -9,22 +14,32 @@ public class TwoSumArraySorted {
         int sum = 0;
         int toReturn0 = 0, toReturn1 = 0;
         while (isFound == false) {
-            sum = numbers[left]+numbers[right];
-            if(sum == target)
-            {
+            sum = numbers[left] + numbers[right];
+            if (sum == target) {
                 toReturn0 = left;
                 toReturn1 = right;
-                isFound = true; 
-                return new int[] {toReturn0, toReturn1};
-            }else if(sum>target)
-            {
+                isFound = true;
+                return new int[] { toReturn0+1, toReturn1+1 };
+            } else if (sum > target) {
                 right--;
-            }else{
+            } else {
                 left++;
             }
         }
 
-        return new int[]{0,0};
+        return new int[] { 0, 0 };
     }
 
+    @Test
+    public void givenArrayReturns() {
+        int[] numbers = { 2, 3, 4 };
+        int target = 6;
+        int[] array = { 1, 3 };
+        Assert.assertArrayEquals(array, this.twoSum(numbers, target));
+
+        int[] numbers2 = { -1, 0 };
+        int[] array2 = { 1, 2 };
+        Assert.assertArrayEquals(array2, this.twoSum(numbers2, -1));
+
+    }
 }
